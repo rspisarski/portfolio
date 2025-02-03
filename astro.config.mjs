63acae900d75860c2accf2822c2542063a4531d0
Richard Pisarski <rspisarski@gmail.com>
2025-02-03 07:42:17 -0500

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ export default defineConfig({
   vite: {
     build: {
       rollupOptions: {
-        external: ['react', 'react-dom', 'framer-motion']
+        external: ['react', 'react-dom']
       }
     }
   }
